c3d227569d6e0726e8d6e1789b57a740689fc35b
epicgamerness <75030939+epicgamerness@users.noreply.github.com>
2021-02-26 16:19:18 -0600

    first test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+require('dotenv').config();
+
 const discord = require('discord.js');
+const client = new discord.Client();
+
+client.db = require('quick.db');
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+
+client.on('ready', async msg => {
+    console.log(
+        `${client.user.tag} = Online (THATS PRETTY POGGERS!)`
+        `${client.user.tag} is also in ${client.guilds.cache.size}`
+    )
+    client.channels.cache.get("814984138381721611").send("Bot is online, pog");
+})
+
+client.login(process.env.TOKEN);
